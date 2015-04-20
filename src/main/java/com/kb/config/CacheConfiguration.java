5ba1bc895c1c99dcaaad11fb43db716fff6bb201
Ivan Ursul <ivan.ursul@optigra-soft.com>
2015-04-20 17:58:36 +0300

    Added serial version uid

--- src/main/java/com/kb/config/CacheConfiguration.java
+++ src/main/java/com/kb/config/CacheConfiguration.java
@@ -1,17 +1,16 @@
 package com.kb.config;
 
+import javax.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.cache.support.NoOpCacheManager; 
-
-import javax.annotation.PreDestroy;
-import javax.inject.Inject;
 
 @Configuration
 @EnableCaching
