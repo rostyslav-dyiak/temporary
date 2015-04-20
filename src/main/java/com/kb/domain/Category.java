4a3728de7a8c0375179ba364be8f529f62feac14
Rostyslav Dyiak <rostyslavdyiak@gmail.com>
2015-04-20 16:41:48 +0300

    Added category entity

---
+++ src/main/java/com/kb/domain/Category.java
@@ -0,0 +1,78 @@
+package com.kb.domain;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Category.
+ */
+@Entity
+@Table(name = "T_CATEGORY")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "parent_id")
+    private Long parentId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Category category = (Category) o;
+
+        if ( ! Objects.equals(id, category.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", title='" + title + "'" +
+                ", parentId='" + parentId + "'" +
+                '}';
+    }
+}
