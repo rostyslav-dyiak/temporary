77f8d9391ca827ade3b8cc989cf34cbfd9d92b83
Oleg Yatskiv <o.yatskiv@optigra.org>
2015-04-24 18:30:30 +0300

    Added method to get Outelts of concrete company.

--- src/main/java/com/kb/repository/OutletRepository.java
+++ src/main/java/com/kb/repository/OutletRepository.java
@@ -1,6 +1,10 @@
 package com.kb.repository;
 
+import com.kb.domain.Company;
 import com.kb.domain.Outlet;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 
 import java.util.List;
@@ -8,6 +12,8 @@ import java.util.List;
 /**
  * Spring Data JPA repository for the Outlet entity.
  */
-public interface OutletRepository extends JpaRepository<Outlet,Long> {
+public interface OutletRepository extends JpaRepository<Outlet, Long> {
+
+	public Page<Outlet> findByCompany(Company company, Pageable page);
 
 }
