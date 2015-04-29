ec571fbdca918bd298dd35fef0614e0cb577e7b4
Oleh Zasadnyy <oleh.zasadnyy@optigra-soft.com>
2015-04-29 10:44:47 +0300

    Refactored Auth service

--- src/main/webapp/js/auth/auth.xauth.service.js
+++ src/main/webapp/js/auth/auth.xauth.service.js
@@ -7,35 +7,75 @@
     AuthServerProvider
         .$inject = [
         '$http',
+        '$q',
         'localStorageService'
     ];
 
-    function AuthServerProvider($http, localStorageService) {
-        return {
-            login: function (email, password) {
-                var data = "email=" + email + "&password="
-                    + password;
-                return $http.post('api/authenticate', data, {
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        "Accept": "application/json"
-                    }
-                }).success(function (response) {
-                    localStorageService.set('token', response);
-                    return response;
-                });
-            },
-            logout: function () {
-                localStorageService.clearAll();
-            },
-            getToken: function () {
-                return localStorageService.get('token');
-            },
-            hasValidToken: function () {
-                var token = this.getToken();
-                return token && token.expires && token.expires > new Date().getTime();
-            }
-        };
-    }
+    function AuthServerProvider($http, $q, localStorageService) {
+        var user;
 
+        var service = {
+            login: login,
+            logout: logout,
+            getToken: getToken,
+            currentUser: currentUser,
+            currentUserCompany: currentUserCompany
+        };
+
+        return service;
+
+        function login(email, password) {
+            var data = "email=" + email + "&password="
+                + password;
+            return $http.post('api/authenticate', data, {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "application/json"
+                }
+            }).success(function (response) {
+                localStorageService.set('token', response);
+                fetchUser().then(
+                    function(data) {
+                        user = data;
+                    }
+                );
+                $http.get("/api/account")
+                    .success(function (data) {
+                        user = data;
+                        def.resolve(data);
+                    });
+                return response;
+            });
+        }
+
+        function logout() {
+            localStorageService.clearAll();
+        }
+
+        function getToken() {
+            return localStorageService.get('token');
+        }
+
+        function currentUser() {
+            return user;
+        }
+
+        function currentUserCompany() {
+            return user.company;
+        }
+
+        function fetchUser() {
+            var def = $q.defer();
+
+            $http.get("/api/account")
+                .success(function (data) {
+                    user = data;
+                    def.resolve(data);
+                })
+                .error(function () {
+                    def.reject("Failed to get user");
+                });
+            return def.promise;
+        }
+    }
 })();
