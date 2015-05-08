ab9f4c30557953cc97894e43f6fe91a6eb21dc5a
Ivan Ursul <ivan.ursul@optigra-soft.com>
2015-05-08 13:59:42 +0300

    Fixed bug

--- src/main/java/com/kb/converter/announcement/SystemAnnouncementDtoConverter.java
+++ src/main/java/com/kb/converter/announcement/SystemAnnouncementDtoConverter.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import com.kb.converter.AbstractConverter;
 import com.kb.domain.SystemAnnouncement;
 import com.kb.domain.SystemAnnouncementUser;
+import com.kb.domain.User;
 import com.kb.web.rest.dto.announcement.SystemAnnouncementDto;
 
 @Component("systemAnnouncementDtoConverter")
@@ -15,16 +16,24 @@ public class SystemAnnouncementDtoConverter extends AbstractConverter<SystemAnno
 
 	@Override
 	public SystemAnnouncement convert(final SystemAnnouncementDto source, final SystemAnnouncement target) {
-
-		Set<SystemAnnouncementUser> users = source.getUsers().stream()
-				.map(id -> new SystemAnnouncementUser(id))
-				.collect(Collectors.toSet());
+		Set<SystemAnnouncementUser> users = null;
+		if (source.getUsers() != null) {
+			users = source.getUsers().stream()
+					.map(id -> {
+						User user = new User();
+						user.setId(id);
+						SystemAnnouncementUser systemAnnouncementUser = new SystemAnnouncementUser();
+						systemAnnouncementUser.setUser(user);
+						return systemAnnouncementUser;
+					})
+					.collect(Collectors.toSet());
+		}
 		
 		target.setUsers(users);
 		target.setAssignmentType(source.getAssignmentType());
 		target.setId(source.getId());
 		target.setIsAdmin(source.getIsAdmin());
-		target.setIsNormalUser(target.getIsNormalUser());
+		target.setIsNormalUser(source.getIsNormalUser());
 		target.setMessage(source.getContent());
 		target.setSubject(source.getSubject());
 		
