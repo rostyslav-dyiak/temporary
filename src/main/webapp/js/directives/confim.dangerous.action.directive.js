9a0b618134d78861fbfdd0603ccc0e14a696be6c
Rostyslav Dyiak <rostyslavdyiak@gmail.com>
2015-05-14 21:44:21 +0300

    Added validation for all dangerous actions in super admin.

--- src/main/webapp/js/directives/confim.dangerous.action.directive.js
+++ src/main/webapp/js/directives/confim.dangerous.action.directive.js
@@ -1,16 +1,19 @@
 /**
  * Created by rdyyak on 14.05.15.
  */
-app.directive('ngConfirmClick', [
-    function () {
+app.directive('ngConfirmClick', ['$ngBootbox',
+    function ($ngBootbox) {
         return {
             link: function (scope, element, attr) {
                 var msg = attr.ngConfirmClick || "Are you sure?";
                 var clickAction = attr.confirmedClick;
                 element.bind('click', function (event) {
-                    if (window.confirm(msg)) {
-                        scope.$eval(clickAction)
-                    }
+                    $ngBootbox.confirm(msg)
+                        .then(function () {
+                            scope.$eval(clickAction);
+                        }, function () {
+                            console.log('Confirm dismissed!');
+                        });
                 });
             }
         };
