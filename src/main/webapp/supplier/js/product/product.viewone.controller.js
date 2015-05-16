3224e89d62d5643b1e13a79e68754c974bad2e01
Rostyslav Dyiak <rostyslavdyiak@gmail.com>
2015-05-16 16:10:10 +0300

    Initial product view fronted and js controller.

--- src/main/webapp/supplier/js/product/product.viewone.controller.js
+++ src/main/webapp/supplier/js/product/product.viewone.controller.js
@@ -10,18 +10,42 @@
     ];
 
     function ViewProductController($scope, toaster, ProductFactory) {
-        $scope.product = {};
+        $scope.product = {
+            productAliasSet: [
+                {
+                    name: 'first myName'
+                },
+                {
+                    name: 'second myName'
+                }
+            ]
+        };
+
+        $scope.removeOtherName = removeOtherName;
+        $scope.addOtherName = addOtherName;
         $scope.products = [];
 
         activate();
 
         function activate() {
-            ProductFactory.query({},
-                function (data) {
-                    $scope.products = data;
-                }, function (e) {
-                    console.error(e);
-                });
+            //ProductFactory.query({},
+            //    function (data) {
+            //        $scope.products = data;
+            //    }, function (e) {
+            //        console.error(e);
+            //    });
+        }
+
+        function removeOtherName(name) {
+            var index = $scope.product.productAliasSet.indexOf(name);
+            $scope.product.productAliasSet.splice(index, 1);
+        }
+
+        function addOtherName() {
+            if (!$scope.product.productAliasSet) {
+                $scope.product.productAliasSet = [];
+            }
+            $scope.product.productAliasSet.push('');
         }
     }
 
